312006fb36120818f6dc4481fa6884d853c499eb
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-08-04 00:53:01 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5700;
+        int rublesForOneBonusMilli = ticketPrice / 20;
+        System.out.println("При стоимости билета в " + ticketPrice + " рублей начисляется " + rublesForOneBonusMilli + " бонусных баллов");
+    }
+}
